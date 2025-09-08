072eb5a1387ee302f7ddafefb5c8bfa2a65b5e78
Vinicios <vinicios.rossi@escola.pr.gov.br>
2025-09-08 11:27:20 -0300

    mensagem

---
+++ 3b.js
@@ -0,0 +1,9 @@
+let temperatura = 19;
+
+if (temperatura >=25){
+    console.log("Quente");A
+} else if (temperatura >= 20){
+    console.log("Agradavel");
+} else {
+    console.log("Frio");
+}
---
+++ 3c.js
@@ -0,0 +1,19 @@
+let dia_da_semana = 5;
+
+if (dia_da_semana <= 0 || dia_da_semana >= 8){
+    console.log("Dia da semana INVÁLIDO");
+} else if (dia_da_semana == 1){
+    console.log("Domingo");
+} else if (dia_da_semana == 2){
+    console.log("Segunda");
+} else if (dia_da_semana == 3){
+    console.log("Terça");
+} else if (dia_da_semana == 4){
+    console.log("Quarta");
+} else if (dia_da_semana == 5){
+    console.log("Quinta");
+} else if (dia_da_semana == 6){
+    console.log("Sexta");
+} else if (dia_da_semana == 7){
+    console.log("Sabado");
+}
